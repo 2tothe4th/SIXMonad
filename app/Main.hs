df3ce0ad6062ff50ed1cdec6307801c1dff80d27
Bravery Aung <104468170+2tothe4th@users.noreply.github.com>
2025-12-01 00:10:34 +0000

    Add some styling

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,28 @@ module Main where
 
 import XMonad
 import XMonad.Util.EZConfig
-import XMONAD.Util.Ungrab
+import XMonad.Util.Ungrab
+
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 
 main :: IO ()
-main = xmonad def
+main = xmonad 
+    . ewmhFullscreen 
+    . ewmh 
+    $ mainConfig
+
+mainConfig :: XConfig (Choose Tall (Choose (Mirror Tall) Full))
+mainConfig = def {
+    modMask = mod4Mask,
+    normalBorderColor = "#202020",
+    focusedBorderColor = "#ffa0ff",
+    borderWidth = 4
+}
+
+ppConfig :: PP
+ppConfig = def {
+
+}
