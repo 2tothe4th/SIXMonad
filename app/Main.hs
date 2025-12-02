d9e6747838d7dd56c20bd55d582509eaec98d4ad
Bravery Aung <onehundredtwentyeight@gmail.com>
2025-12-02 18:46:50 -0500

    Add a lot

--- app/Main.hs
+++ app/Main.hs
@@ -9,20 +9,28 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
+import XMonad.Layout.NoBorders
+
 main :: IO ()
 main = xmonad 
     . ewmhFullscreen 
     . ewmh 
     $ mainConfig
 
-mainConfig :: XConfig (Choose Tall (Choose (Mirror Tall) Full))
+-- mainConfig :: XConfig (Choose Tall (Choose (Mirror Tall) (ModifiedLayout WithBorder Full)))
 mainConfig = def {
     terminal = "kitty",
     modMask = mod4Mask,
     normalBorderColor = "#202020",
     focusedBorderColor = "#ffc0ff",
-    borderWidth = 4
+    focusFollowsMouse = False,
+    clickJustFocuses = False,
+    borderWidth = 4,
+    layoutHook = mainLayout
 }
+
+mainLayout = half ||| Mirror half ||| smartBorders Full where
+    half = Tall 1 (3/100) (1/2)
 
 ppConfig :: PP
 ppConfig = def
